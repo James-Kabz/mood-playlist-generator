d180834fad546d6b0e8914c57f0277ff7f380ab6
James-Kabz <kabogp@gmail.com>
2025-04-04 08:13:41 +0300

    openai fix

--- src/lib/openai/openai.ts
+++ src/lib/openai/openai.ts
@@ -1,10 +1,8 @@
-import dotenv from "dotenv"
 import OpenAI from "openai"
 
-dotenv.config()
 // Initialize the OpenAI client
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY || process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 })
 
 export default openai
